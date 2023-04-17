893821551609d5415bae1ebae143eb446e7add7d
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-04-17 13:46:17 +0500

    path

---
+++ path-parameters/myapi.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI  , Path
+
+
+app=FastAPI()
+
+@app.get("/")
+def index():
+    return {'name': 'Abu Bakar Waheed'}
+
+
+# Path Parameters
+
+student={
+    1:{
+        'name':'abu bakar waheed',
+        'program':'Bs Computer Science',
+        'smester':'5',
+        'age':'21',
+    },
+    2:{
+        'name':'Amir',
+        'program':'Bs Computer Science',
+        'smester':'1',
+        'age':'21',
+    },
+    3:{
+        'name':'Ali Hamza',
+        'program':'Bs Computer Science',
+        'smester':'1',
+        'age':'24',
+    },
+            
+    4:{
+        'name':'Abdul rehman',
+        'program':'Bs Computer Science',
+        'smester':'1',
+        'age':'27',
+    },
+}
+
+@app.get("/get_student/{st_id}")
+def student_data(
+    st_id: int = Path( description="The id of the student you want to view" , gt=0)
+    ):
+    return student[st_id]
